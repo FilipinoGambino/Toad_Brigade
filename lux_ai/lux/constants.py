cd8fa246fc3e131b9ff55f49f6677bad21771797
Nicholas Gorichs <ngorichs1@gmail.com>
2023-03-23 16:33:09 -0500

    .

--- lux_ai/lux/constants.py
+++ lux_ai/lux/constants.py
@@ -11,19 +11,19 @@ class Constants:
         DONE = "D_DONE"
 
     class DIRECTIONS:
-        NORTH = "n"
-        WEST = "w"
-        SOUTH = "s"
-        EAST = "e"
-        CENTER = "c"
+        CENTER = 0
+        UP = 1
+        RIGHT = 2
+        DOWN = 3
+        LEFT = 4
 
         @staticmethod
         def astuple(include_center: bool):
             move_directions = (
-                Constants.DIRECTIONS.NORTH,
-                Constants.DIRECTIONS.EAST,
-                Constants.DIRECTIONS.SOUTH,
-                Constants.DIRECTIONS.WEST
+                Constants.DIRECTIONS.UP,
+                Constants.DIRECTIONS.DOWN,
+                Constants.DIRECTIONS.LEFT,
+                Constants.DIRECTIONS.RIGHT,
             )
             if include_center:
                 return move_directions + (Constants.DIRECTIONS.CENTER,)
@@ -32,15 +32,21 @@ class Constants:
 
     # noinspection PyPep8Naming
     class UNIT_TYPES:
-        WORKER = 0
-        CART = 1
+        LIGHT = 0
+        HEAVY = 1
 
     # noinspection PyPep8Naming
     class RESOURCE_TYPES:
-        WOOD = "wood"
-        URANIUM = "uranium"
-        COAL = "coal"
+        ICE = 1
+        WATER = 2
+        ORE = 3
+        METAL = 4
 
         @staticmethod
         def astuple():
-            return Constants.RESOURCE_TYPES.WOOD, Constants.RESOURCE_TYPES.COAL, Constants.RESOURCE_TYPES.URANIUM
+            return (
+                Constants.RESOURCE_TYPES.ICE,
+                Constants.RESOURCE_TYPES.WATER,
+                Constants.RESOURCE_TYPES.ORE,
+                Constants.RESOURCE_TYPES.METAL,
+            )
